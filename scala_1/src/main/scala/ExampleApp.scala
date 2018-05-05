214a8202eee207146ee2352fbb3140c027833c6d
Xiaotian Bai <xiaotianbai0206@gmail.com>
2018-05-04 21:26:21 -0400

    spark machine learning project added

--- scala_1/src/main/scala/ExampleApp.scala
+++
@@ -1,9 +0,0 @@
-
-object ExampleApp extends App{
-  def abs(x: Double) = if(x >= 0) x else -x
-  print(abs(-3))
-}
-object Session{
-  1+2
-
-}
--- scala_1/src/main/scala/example.sc
+++
@@ -1,3 +0,0 @@
-"Hello, world!"
-def abs(x: Double) = if (x >= 0) x else -x
-abs(-3)
